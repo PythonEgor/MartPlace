09dcfba6d085db5e46c26dd9fac724990222cf1e
PythonEgor <lyasovich90@mail.ru>
2020-10-16 13:44:45 +0300

    creating the main page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,31 @@
 $(function(){
 
+    $('.slider-top__inner').slick({
+        dots: false,
+        arrows: true,
+    });
+
+    $('.slider-bottom__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true,
+        spacing: "2px",
+    });
+
+    $(".products-star").rateYo({
+        rating: 4.5,
+        starWidth: "12px",
+        readOnly: true,
+        spacing: "2px",
+    });
     
 
+    var mixer=mixitup('.products__inner-box');
 });
